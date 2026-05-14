a8bd288c1a095c1c78d9dad760a27bc8b26a97ce
legop3 <minecratfd8@gmail.com>
2026-05-13 21:55:41 -0400

    change method

--- player.js
+++ player.js
@@ -62,7 +62,7 @@ function playWithMpv(filePath, displayConfig) {
     const args = [];
     if (displayConfig && displayConfig.fullscreen) args.push('--fs');
     if (displayConfig && Number.isInteger(displayConfig.screen)) args.push(`--screen=${displayConfig.screen}`);
-    args.push('--af=dynaudnorm');
+    args.push('--af=loudnorm');
     args.push(filePath);
 
     const proc = spawn('mpv', args, { stdio: 'inherit' });
